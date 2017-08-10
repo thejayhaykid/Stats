e1df9fe264d25db0131a9ebd92bdf24249594fd5
Jake Hayes <jayhay28324@gmail.com>
2017-08-10 12:17:46 -0400

    Updated game

--- src/game.java
+++ src/game.java
@@ -1,6 +1,12 @@
 
 public class game {
-
+	team homeTeam, awayTeam;
+	
+	public game(team home, team away) {
+		this.homeTeam = home;
+		this.awayTeam = away;
+	}
+	
 	public game() {
 		// TODO Auto-generated constructor stub
 	}
--- src/team.java
+++ src/team.java
@@ -3,6 +3,12 @@ import java.util.HashSet;
 public class team {
 
 	HashSet<player> players;
+	String name;
+	
+	public team(String _name) {
+		players = new HashSet<player>();
+		this.name = _name;
+	}
 	
 	public team() {
 		players = new HashSet<player>();
