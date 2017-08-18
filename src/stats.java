94656c749150b876003fc053fc8844b1cff4ad39
Jake Hayes <jayhay28324@gmail.com>
2017-08-18 15:10:20 -0400

    ran applet tests and failed. Adjusting course

--- src/stats.java
+++ src/stats.java
@@ -1,5 +1,6 @@
 import java.applet.Applet;
 import java.awt.Graphics;
+import java.util.Scanner;
 
 
 @SuppressWarnings("unused")
@@ -15,6 +16,13 @@ public class stats extends Applet {
 	}
 
 	public static void main(String[] args) {
+		// Testing for applet purposes
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String test = in.nextLine();
+		System.out.println("Your string was " + test);
+		in.close();
+		
 		// TODO Create/load teams
 		
 		// TODO Create game()
