fe6862ecc65ce028331d76502d1901291c69cc27
Jake Hayes <jayhay28324@gmail.com>
2017-08-21 20:12:45 -0400

    bug fixes for file export

--- src/team.java
+++ src/team.java
@@ -1,5 +1,6 @@
 import java.io.PrintWriter;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class team {
@@ -77,7 +78,46 @@ public class team {
 			out.println(maxPrepsID);
 		}
 		
+		out.println("Jersey|PassingComp|PassingAtt|PassingYards|PassingTD|PassingInt|PassingLong|RushingNum|RushingYards|RushingLong|RushingTDNum|ReceivingNum|ReceivingYards|ReceivingLong|ReceivingTDNum|OffensiveFumbesLost|PancakeBlocks|Tackles|Assists|Sacks|TacklesForLoss|CausedFumbles|FumbleRecoveries|FumbleRecoveryYards|FumbleReturnedTDNum|Safeties|INTs|INTYards|PassesDefensed|IntReturnedTDNum");
 		
+		String p = "|"; //Pipe for delimiter
+		
+		Iterator<player> it = this.players.iterator();
+		player temp = it.next();
+		
+		do {
+			out.print(temp.number + p);
+			out.print(temp.passComp + p);
+			out.print(temp.passAtt + p);
+			out.print(temp.passYds + p);
+			out.print(temp.passTD + p);
+			out.print(temp.passInt + p);
+			out.print(temp.passLong + p);
+			out.print(temp.rushAtt + p);
+			out.print(temp.rushYds + p);
+			out.print(temp.rushLong + p);
+			out.print(temp.rushTD + p);
+			out.print(temp.recComp + p);
+			out.print(temp.recYds + p);
+			out.print(temp.recLong + p);
+			out.print(temp.recTD + p);
+			out.print(temp.rushFum + p);
+			out.print(temp.bloPan + p);
+			out.print(temp.tkl + p);
+			out.print(temp.tklAss + p);
+			out.print(temp.tklSck + p);
+			out.print(temp.tklLss + p);
+			out.print(temp.fumFor + p);
+			out.print(temp.fumRec + p);
+			out.print(temp.fumYds + p);
+			out.print(temp.fumTD + p);
+			out.print(temp.safety + p);
+			out.print(temp.intRec + p);
+			out.print(temp.intYds + p);
+			out.print(temp.pssBrk + p);
+			out.print(temp.intTD + "\n");
+			temp = it.next();
+		} while (it.hasNext());
 	}
 
 }
